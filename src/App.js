8bac6ec1cacd49a51f91e8cdd5e2db247da11c7d
unknown <minh01645720374@gmail.com>
2025-01-11 16:47:49 +0700

    Init project again

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-red-500 p-10">
+      <h1 className="text-3xl font-bold">Hello Tailwind CSS!</h1>
+      <p className="text-lg mt-2">Dự án React với Tailwind CSS đã sẵn sàng!</p>
+    </div>
+  );
+}
+
+export default App;
