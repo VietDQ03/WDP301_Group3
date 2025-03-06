d4453748fd440f6cbab56400745bbb838c3b687d
tuantran180202 <trananhtuan180202@gmail.com>
2025-03-06 21:12:55 +0700

    update code

--- src/api/UserApi/UserApi.js
+++ src/api/UserApi/UserApi.js
@@ -10,6 +10,8 @@ export const callCreateResume = (url, companyId, jobId, description) => {
     });
 };
 
+
+
 export const callUploadSingleFile = (file, folderType) => {
     const bodyFormData = new FormData();
     bodyFormData.append("fileUpload", file);
