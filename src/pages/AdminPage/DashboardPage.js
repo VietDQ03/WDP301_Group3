8d3bec6daba89525ff29b7388785353c92ce32d8
tuantran180202 <trananhtuan180202@gmail.com>
2025-03-08 23:36:09 +0700

    Update DashboardPage.js

--- src/pages/AdminPage/DashboardPage.js
+++ src/pages/AdminPage/DashboardPage.js
@@ -14,8 +14,6 @@ const { Content } = Layout;
 
 
 
-
-
 const DashboardPage = () => {
   const [collapsed, setCollapsed] = React.useState(false);
   const [dataCompanies, setDataCompanies] = useState([]);
